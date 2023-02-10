7421525530f44f8a2e137c1b8c0eb59841b27b0e
tuan304201 <tuananh30042001@gmail.com>
2023-02-10 13:50:43 +0700

    design trello front-end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,96 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="app__container">
+            <nav className="navbar app">App bar</nav>
+            <nav className="navbar board">Board bar</nav>
+            <div className="board__columns">
+                <div className="column">
+                    <header>title content</header>
+                    <ul>
+                        <li>
+                            <img
+                                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEVZGoUnd3DHYJhY9R5n0UHpyV96r4XUwIsw&usqp=CAU"
+                                alt=""
+                            />
+                            Title: Haa
+                        </li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+
+                <div className="column">
+                    <header>title content</header>
+                    <ul>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+
+                <div className="column">
+                    <header>title content</header>
+                    <ul>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+
+                <div className="column">
+                    <header>title content</header>
+                    <ul>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+
+                <div className="column">
+                    <header>title content</header>
+                    <ul>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+
+                <div className="column">
+                    <header>title content</header>
+                    <ul>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                        <li>hahahahaah</li>
+                    </ul>
+                    <footer>Add another card</footer>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
